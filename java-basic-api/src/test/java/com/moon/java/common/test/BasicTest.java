76810f866acae1a7f5eabce3b3c5741990ea6da1
MooNkirA <lje888@126.com>
2021-03-11 21:43:22 +0800

    feat: Java类初始化顺序示例

---
+++ java-basic-api/src/test/java/com/moon/java/common/test/BasicTest.java
@@ -0,0 +1,32 @@
+package com.moon.java.common.test;
+
+import com.moon.java.basic.InitSequenceBean;
+import org.junit.Test;
+
+/**
+ * Java 基础测试
+ *
+ * @author MooNkirA
+ * @version 1.0
+ * @date 2021-3-9 23:00
+ * @description
+ */
+public class BasicTest {
+
+    /* 测试Java类初始化顺序 */
+    @Test
+    public void testInitializationSequence() {
+        InitSequenceBean bean = new InitSequenceBean();
+        System.out.println(bean);
+        /*
+         * 测试结果：
+         *  静态成员变量初始化....
+         *  静态代码块执行了....
+         *  普通成员变量初始化....
+         *  初始化代码块执行了....
+         *  无参构造函数执行了....
+         *  com.moon.java.basic.InitSequenceBean@ba8a1dc
+         */
+    }
+
+}
