04efd46716eda4e7322bd7f327776fd1af27d6ff
MooNkirA <lje888@126.com>
2022-09-21 19:15:53 +0800

    feat: drools 规则基础使用示例

--- java-stack-drools/drools-quickstart/src/test/java/com/moon/drools/test/DroolsTest.java
+++ java-stack-drools/drools-quickstart/src/test/java/com/moon/drools/test/DroolsTest.java
@@ -1,10 +1,14 @@
 package com.moon.drools.test;
 
+import com.moon.drools.entity.ComparisonOperatorEntity;
 import com.moon.drools.entity.Order;
 import org.junit.Test;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Drools 测试
@@ -16,8 +20,9 @@ import org.kie.api.runtime.KieSession;
  */
 public class DroolsTest {
 
+    // 基础使用
     @Test
-    public void test1(){
+    public void test1() {
         // 获取 KieServices
         KieServices kieServices = KieServices.Factory.get();
         // 获得 KieContainer（容器）对象
@@ -31,15 +36,41 @@ public class DroolsTest {
 
         // 将数据提供给规则引擎（放入工作内存中），规则引擎会根据提供的数据进行规则匹配
         session.insert(order);
-
-        System.out.println("----优惠后价格："+order.getRealPrice());
+        System.out.println("没有激活规则前，优惠价格属性是：" + order.getRealPrice());
 
         // 激活规则，由Drools框架自动进行规则匹配，如果规则匹配成功，则执行当前规则
         session.fireAllRules();
 
         // 关闭会话
         session.dispose();
-        System.out.println("优惠后价格："+order.getRealPrice());
+        System.out.println("优惠后价格：" + order.getRealPrice());
+    }
+
+    // 测试使用比较操作符
+    @Test
+    public void test2() {
+        // 获取 KieServices
+        KieServices kieServices = KieServices.Factory.get();
+        // 获得 KieContainer（容器）对象
+        KieContainer kieContainer = kieServices.newKieClasspathContainer();
+        // 从 KieContainer（容器）对象中获取会话对象，用于和规则引擎交互
+        KieSession session = kieContainer.newKieSession();
+
+        // 构造订单对象（Fact对象，事实对象），设置原始价格，由规则引擎根据优惠规则计算优惠后的价格
+        ComparisonOperatorEntity fact = new ComparisonOperatorEntity();
+        fact.setNames("李四");
+        List<String> list = new ArrayList<>();
+        list.add("张三2");
+        fact.setList(list);
+
+        // 将数据提供给规则引擎（放入工作内存中），规则引擎会根据提供的数据进行规则匹配
+        session.insert(fact);
+
+        // 激活规则，由Drools框架自动进行规则匹配，如果规则匹配成功，则执行当前规则
+        session.fireAllRules();
+
+        // 关闭会话
+        session.dispose();
     }
 
 }
